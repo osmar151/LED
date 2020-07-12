b39a52d49c40d067fc87e48b1a430cf88a1048e7
osmar151 <62614167+osmar151@users.noreply.github.com>
2020-07-11 21:17:58 -0600

    proyecto terminado

--- e LED/Program.cs
+++ e LED/Program.cs
@@ -8,9 +8,12 @@ namespace e_LED
 {
     class Program
     {
+        // integrantes: Giuliana, Lineli, Isabel y Osmar
+        //Link de programa de timkercad: https://www.tinkercad.com/things/fhzcfgRpY3h-copy-of-proyecto/editel?sharecode=jDVVtDaxlcMv5CfS0_lS0vUen1Q22n_CXrW8Jvmdwhs
+
         static char[,] datos = new char[3, 3];
-        static SerialPort Arduino = new SerialPort("COM5, 9600"); // en la parte de COM5 que esta a la izquieda
-                                                                  //poner el que diga Arduino.
+        static SerialPort Arduino = new SerialPort("COM5", 9600); // cambiar COM5 por puerto de arduino predeterminado
+                                                                 
         static void Main(string[] args)
         {
             char menu = 'n';
@@ -24,13 +27,20 @@ namespace e_LED
 
             Arduino.Open();
 
+            Console.WriteLine(" Bienvenido, este programa fue creado para manipular las luces de un semaforo," +
+                " donde el usuario puede seleccionar que luz desea encender "); 
+
             while (menu != 's')
 
             {
+                Console.WriteLine(" \\ n ---------------------------------- \\ n ");
+                Console.WriteLine(" _______________________________________________________________ ");
                 Console.WriteLine(" 1.-encender luz verde ");
                 Console.WriteLine(" 2.-encender luz amarilla ");
                 Console.WriteLine(" 3.-encender luz roja ");
                 Console.WriteLine(" 4.-salir ");
+                Console.WriteLine(" \\ n ---------------------------------- \\ n ");
+                Console.WriteLine(" seleccione la opcion que desea ejecutar: ");
 
                 op = int.Parse(Console.ReadLine());
 
@@ -39,15 +49,20 @@ namespace e_LED
                 {
                     if (Arduino.IsOpen)
                     {
+                        Console.WriteLine(" \\ n ---------------------------------- \\ n ");
                         Console.WriteLine("com abierto");
-                        byte[] data = Encoding.ASCII.GetBytes("Ve");
+                        byte[] data = Encoding.ASCII.GetBytes("V");
                         Arduino.Write(data, 0, data.Length);
 
-                        Arduino.Write("Vp");
+                        Console.WriteLine(" ingrese: 'V' para encender la luz verde");
+                        Console.WriteLine(" ingrese: 'A' para apagar la luz verde");
+
+                        Arduino.Write("A");
                     }
 
                     else
                     {
+                        Console.WriteLine(" \\ n ---------------------------------- \\ n ");
                         Console.WriteLine("com cerrado");
                     }
                 }
@@ -56,15 +71,20 @@ namespace e_LED
                 {
                     if (Arduino.IsOpen)
                     {
+                        Console.WriteLine(" \\ n ---------------------------------- \\ n ");
                         Console.WriteLine("com abierto");
-                        byte[] data = Encoding.ASCII.GetBytes("Ae");
+                        byte[] data = Encoding.ASCII.GetBytes("B");
                         Arduino.Write(data, 0, data.Length);
 
-                        Arduino.Write("Ap");
+                        Console.WriteLine(" ingrese: 'B' para encender la luz amarilla");
+                        Console.WriteLine(" ingrese: 'C' para apagar la luz amarila");
+
+                        Arduino.Write("C");
                     }
 
                     else
                     {
+                        Console.WriteLine(" \\ n ---------------------------------- \\ n ");
                         Console.WriteLine("com cerrado");
                     }
                 }
@@ -73,15 +93,20 @@ namespace e_LED
                 {
                     if (Arduino.IsOpen)
                     {
+                        Console.WriteLine(" \\ n ---------------------------------- \\ n ");
                         Console.WriteLine("com abierto");
-                        byte[] data = Encoding.ASCII.GetBytes("Re");
+                        byte[] data = Encoding.ASCII.GetBytes("D");
                         Arduino.Write(data, 0, data.Length);
 
-                        Arduino.Write("Rp");
+                        Console.WriteLine(" ingrese: 'D' para encender la luz roja");
+                        Console.WriteLine(" ingrese: 'E' para apagar la luz roja");
+
+                        Arduino.Write("E");
                     }
 
                     else
                     {
+                        Console.WriteLine(" \\ n ---------------------------------- \\ n ");
                         Console.WriteLine("com cerrado");
                     }
 
@@ -95,9 +120,10 @@ namespace e_LED
 
                 }
 
-
-                    Console.ReadKey();
+                Console.ReadKey();
             }
+
+            Arduino.Close();
         }
     }
 }
